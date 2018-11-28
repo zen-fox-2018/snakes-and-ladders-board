e9998b6a0b7889008a2b7a6e669b6cd3d3761b4b
rahmanhasri <rahman.hasri@gmail.com>
2018-11-28 15:02:03 +0700

    maman ular tangga

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 //your code here
+function ularTangga(x){
+  let output = []
+  let counter = x * x
+
+  for (let i = 0 ; i < x ; i++){
+    let arrDalam = []
+
+    if ((x-i) % 2 === 0) {
+
+      for (let j = 0 ; j < x ; j++) {
+        arrDalam[j] = counter
+        counter--
+      }
+
+    } else {
+
+      for (let j =  x-1 ; j >= 0 ; j--){
+        arrDalam[j] = counter
+        counter--
+      }
+
+    }
+    output.push(arrDalam)
+  }
+  return output
+}
+
+console.log(ularTangga(15));
