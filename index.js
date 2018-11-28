f129a7982322a4d913d1bc00ce0eee99d68fd7f2
Aditya Respati <AdityaRespati>
2018-11-28 19:23:28 +0700

    added solution

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //your code here
+
+function generateBoard(n) {
+    let arrOutput = []
+
+    for (let i = n; i > 0; i--) {
+        let arrRow = []
+
+        for (let j = 0; j < n ; j++) {
+            if (i % 2 === 0) {
+                arrRow.push(n*i - j)
+            }   else{
+                arrRow.push(n*(i-1) + (j+1))
+            }
+        }
+        arrOutput.push(arrRow)
+    }
+    return arrOutput
+}
+
+console.log(generateBoard(8))
