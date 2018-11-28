bd267afb6f0eed6ab65d01b176df8466e08abc11
Luthfi Arfiansyah <luthfi.arfi@gmail.com>
2018-11-28 14:08:33 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //your code here
+function generateBoard(num){
+  var total = num * num;
+  var result = [];
+  for (var i = 0; i < num; i++) {
+    var fillResult = [];
+    for (var j = 0; j < num; j++) {
+      fillResult.push(total);
+      total--;
+    }
+    result.push(fillResult);
+  }
+  return result
+}
+
+console.log(generateBoard(15));
