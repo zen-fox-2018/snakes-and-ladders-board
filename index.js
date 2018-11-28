d60c955b41fa9cd6134f229c4ae07a46d69089c2
Celyn Susanto <celyn.vy@gmail.com>
2018-11-28 13:56:22 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //your code here
+function snakeAndLadder (dimension) {
+    var number = dimension * dimension;
+    var result = [];
+    for (var i = 0; i < dimension; i++) {
+        var temp = [];
+        for (var j = 0; j < dimension; j++) {
+            if (dimension % 2 == 0 && i % 2 != 0) {
+                temp[(dimension-1)-j] = number;
+            } else if (dimension % 2 !=0 && i % 2  == 0) {
+                temp[(dimension-1)-j] = number;
+            }else {
+                temp.push (number);
+            }
+            number--;
+        }
+        result.push (temp);
+    }
+    return result;
+}
+console.log (snakeAndLadder (15));
+console.log (snakeAndLadder (8));
