8b062af4c6ec7aa27192bf00e50d05b762aff2b5
ShadowM8 <anton2014lenovo@gmail.com>
2018-11-28 13:55:24 +0700

    snake and leader board done -anton-

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-//your code here
+
+function generateBoard(num = 10) {
+    let hasil = []
+
+    for (let i = num - 1; i >= 0; i--) {
+        let hasilDalam = []
+        if (i % 2 === 0) {
+            for (let j = 0; j < num; j++) {
+                var angka = (j + 1 + i * num)
+                hasilDalam.push(angka)
+
+            }
+            hasil.push(hasilDalam)
+        }
+        else {
+            for (let j = num - 1; j >= 0; j--) {
+                var angka = (j + 1 + i * num)
+                hasilDalam.push(angka)
+
+            }
+            hasil.push(hasilDalam)
+        }
+
+    }
+    return hasil
+}
+
+console.log(generateBoard())
+console.log(generateBoard(15))
+console.log(generateBoard(8))
