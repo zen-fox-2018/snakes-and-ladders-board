66959c913612f9f8978a8c3f971e6f068ca204e4
maitreya <bluespider529@gmail.com>
2018-11-28 14:34:43 +0700

    done snakes and ladders

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//your code here
+function generateBoard(number) {
+    let board = [];
+    let total = number * number;
+
+    for(let i = 0; i < number; i++) {
+        board.push([]);
+        let count = number-1;
+        for(let j = 0; j < number; j++) {
+            if(i%2 === 0 && number%2 === 0 || i%2 === 1 && number%2 === 1) {
+                board[i].push(total);
+            } else {
+                board[i][count] = total;
+                count--;
+            }
+            total--;
+        }
+    }
+    return board;
+}
+console.log(generateBoard(10));
+console.log(generateBoard(5));
