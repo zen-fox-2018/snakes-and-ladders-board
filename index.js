c3cd94efc3dbdb157f8432b6130eb2917e5b58a2
ahmad <ahmad.syukron6991@gmail.com>
2018-11-28 16:15:31 +0700

    snake n ladder done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //your code here
+function snakeNladder(num){
+    var result = []
+    for(let i = num-1; i >= 0; i --){
+        var holder = []
+        if(i % 2 == 0){
+            for(let j = 1; j <= num; j ++){
+            holder.push((i*num)+j)
+                }
+            }
+        else{
+            for(let j = num; j >= 1; j-- ){
+            holder.push((i*num)+j)
+        }
+    }
+    result.push(holder)
+}
+    return result
+}
+console.log(snakeNladder(15))
