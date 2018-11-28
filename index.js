ce6b0252041700bcb51b28fb4292e5858ae3a77e
ACER <fauziaazia171097@gmail.com>
2018-11-28 03:09:40 -0500

    finish

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //your code here
+function generateboard(input) {
+    let num = input*input
+    let result = []
+    for (let i = input ; i > 0 ; i--) {
+        let arr = []
+        let count = input -1
+        for (j = 0; j < input ; j++) {
+            if (i% 2 === 0) {
+                arr.push(num)
+            } else {
+                arr[count] = num
+                count--
+            }
+            num--
+        }
+        result.push(arr)
+    }
+
+    return result
+}
+
+console.log(generateboard(15))
+console.log(generateboard(4))
